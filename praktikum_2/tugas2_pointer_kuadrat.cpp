cb6bc0f590ae642421e7b7b499f620b13a9d1986
alexanderl4 <limbonghotman02@gmail.com>
2026-03-04 18:51:03 +0700

    Create tugas2_pointer_kuadrat.cpp

---
+++ praktikum_2/tugas2_pointer_kuadrat.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// # Fungsi void dengan parameter pointer
+void kuadratkan(int *val) {
+    *val = (*val) * (*val); // # Mengubah nilai di alamat memori secara langsung
+}
+
+int main() {
+    int angka = 5; // # Deklarasi variabel
+
+    cout << "Nilai awal : " << angka << endl;
+    
+    // # Memanggil fungsi dengan mengirimkan alamat memori (&)
+    kuadratkan(&angka); 
+    
+    cout << "Nilai akhir: " << angka << endl; // # Nilai otomatis berubah jadi 25
+
+    return 0;
+}
